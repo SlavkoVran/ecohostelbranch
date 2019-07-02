59425f18d36828edd8a31bbff1c22672933ebb78
SlavkoVran <slavkov.mob@gmail.com>
2019-07-02 20:46:16 +0200

    gitignore

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,17 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Eco</span>
+        <span class="font-weight-light">Hostel Branch</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <v-btn to="/home" class="mr-2">Home</v-btn>
+      <v-btn to="/rooms" class="mr-2">Rooms</v-btn>
+      <v-btn to="/what" class="mr-2">What to see?</v-btn>
+      <v-btn to="/event" class="mr-2">Event</v-btn>
+      <v-btn to="/gallery" class="mr-2">Gallery</v-btn>
+      <v-btn to="/contact" class="mr-2">Contact</v-btn>
     </v-toolbar>
-
     <v-content>
       <HelloWorld/>
     </v-content>
